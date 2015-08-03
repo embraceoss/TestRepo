5838951c92956ffcab85ed565cd5d93111c08f89
testaccount67 <nirajg@nirajg-dev2.redmond.corp.microsoft.com>
2015-08-02 19:43:53 -0700

    Sample xunit test

---
+++ src/Testing2/ProductCode/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProductCode
+{
+    public class Class1
+    {
+        public Service s;
+
+        public Class1()
+        {
+            s = new Service();
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, s.Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, s.Add(2, 2));
+        }
+    }
+}
